9015d245d95278ac8e845b41263ebf596b3e6e59
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-26 21:53:27 +0100

    :construction_worker: (ansible) setup

---
+++ server.js
@@ -0,0 +1,15 @@
+// app.js
+
+const express = require("express");
+const app = express();
+
+// Define a route for the root URL ("/")
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the server and listen on port 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
